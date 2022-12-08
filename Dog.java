a99e997637fda5b32de517a6bf0fb57540ec9d5d
BRHS-PLTW-01 <549974@my.cuhsd.org>
2022-12-08 13:11:35 -0800

    Final commit, all rubric requirements satisfied. ✓

--- Dog.java
+++ Dog.java
@@ -2,14 +2,13 @@
  * @author Jake A
  * @version 1
  */
-
 import java.util.Scanner;
 class Dog {
   private int weight;
   private int age;
 
   /**
-   * Constructor, creates a dog instance
+   * Object class with Constructor ✓, creates a dog instance
    * @param w weight in lbs
    * @param a age in years
    */
@@ -24,13 +23,13 @@ class Dog {
    * @param mi distance(miles)
    * @return if dog has been walked enough, or if the dog needs to keep walking.
    */
-  public String exercise(double mi) {
+  public String exercise(double mi) { // Method with parameters ✓
     boolean enough = false;
-    while(!enough) {
+    while(!enough) { // Iteration (loop) ✓
       if(age >= 15 && mi >= 0.75) {
         enough = true;
         return "You walked far enough! Your dog thanks you.";
-      }
+      } // Advanced Conditionals below ✓
       if((weight<25 && mi >= 0.75)||(weight < 50 && mi >= 1.25)||(weight < 75 && mi >= 2.0)||(weight<110&&mi>= 3.0)||(weight>150&&mi>4.5)) {
         enough = true;
         return "You walked far enough! Your dog thanks you.";
--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 /**
  * Dog walking program (Turner final)
  * @author Jake A
- * @version 1
+ * @version 1.0
+ * @since 2022-12-08
  */
 
 import java.util.Scanner;
@@ -10,16 +11,16 @@ class Main {
    * Asks for user input, and returns if the dog has been walked enough or not.
    * Prints the output of Dog's exercise method
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) { // Main method ✓
     Scanner input = new Scanner(System.in);
     System.out.println("How much does your dog weigh?");
-    double w = input.nextDouble();
+    double w = input.nextDouble(); // User input ✓
     System.out.println("How old is your dog?");
     int a = input.nextInt();
     System.out.println("How far did you walk? (miles)");
     double mi = input.nextDouble();
     Dog d = new Dog(w, a);
-    System.out.println(d.exercise(mi));
+    System.out.println(d.exercise(mi)); // Program output ✓
     input.close();
   }
 }
